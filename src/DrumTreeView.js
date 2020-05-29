9cc4e69d6c81c13dd497a639fef59461327e56fc
alexrichardson21 <richardson.alex101@gmail.com>
2020-05-29 12:43:36 -0700

    new branch

--- src/DrumTreeView.js
+++ src/DrumTreeView.js
@@ -1,210 +1,210 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TreeView from '@material-ui/lab/TreeView';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import TreeItem from '@material-ui/lab/TreeItem';
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TreeView from "@material-ui/lab/TreeView";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import TreeItem from "@material-ui/lab/TreeItem";
+const data =
 const data = [
-    {
-      id: 'Kicks',
-      name: 'Kicks',
-      children: [
-        {
-          id: '808 Trap',
-          name: '808 Trap',
-        },
-        {
-          id: '909 House',
-          name: '909 House',
-        },
-        {
-          id: 'Sampled',
-          name: 'Sampled',
-        },
-        {
-          id: 'Lofi',
-          name: 'Lofi',
-        },
-      ]
-    },
-    {
-      id: 'Snares',
-      name: 'Snares',
-      children: [
-        {
-          id: '808 Trap',
-          name: '808 Trap',
-        },
-        {
-          id: '909 House',
-          name: '909 House',
-        },
-        {
-          id: 'Sampled',
-          name: 'Sampled',
-        },
-        {
-          id: 'Lofi',
-          name: 'Lofi',
-        },
-      ]
-    },
-    {
-      id: 'Claps',
-      name: 'Claps',
-      children: [
-        {
-          id: '808 Trap',
-          name: '808 Trap',
-        },
-        {
-          id: '909 House',
-          name: '909 House',
-        },
-        {
-          id: 'Sampled',
-          name: 'Sampled',
-        },
-        {
-          id: 'Lofi',
-          name: 'Lofi',
-        },
-      ]
-    },
-    {
-      id: 'Snaps',
-      name: 'Snaps',
-      children: [
-        {
-          id: '808 Trap',
-          name: '808 Trap',
-        },
-        {
-          id: '909 House',
-          name: '909 House',
-        },
-        {
-          id: 'Sampled',
-          name: 'Sampled',
-        },
-        {
-          id: 'Lofi',
-          name: 'Lofi',
-        },
-      ]
-    },
-    {
-      id: 'ClosedHats',
-      name: 'Closed Hats',
-      children: [
-        {
-          id: '808 Trap',
-          name: '808 Trap',
-        },
-        {
-          id: '909 House',
-          name: '909 House',
-        },
-        {
-          id: 'Sampled',
-          name: 'Sampled',
-        },
-        {
-          id: 'Lofi',
-          name: 'Lofi',
-        },
-      ]
-    },
-    {
-      id: 'OpenHats',
-      name: 'Open Hats',
-      children: [
-        {
-          id: '808 Trap',
-          name: '808 Trap',
-        },
-        {
-          id: '909 House',
-          name: '909 House',
-        },
-        {
-          id: 'Sampled',
-          name: 'Sampled',
-        },
-        {
-          id: 'Lofi',
-          name: 'Lofi',
-        },
-      ]
-    },
-    {
-      id: 'Cymbals',
-      name: 'Cymbals',
-      children: [
-        {
-          id: '808 Trap',
-          name: '808 Trap',
-        },
-        {
-          id: '909 House',
-          name: '909 House',
-        },
-        {
-          id: 'Sampled',
-          name: 'Sampled',
-        },
-        {
-          id: 'Lofi',
-          name: 'Lofi',
-        },
-      ]
-    },
-    {
-      id: 'Percs',
-      name: 'Percs',
-      children: [
-        {
-          id: '808 Trap',
-          name: '808 Trap',
-        },
-        {
-          id: '909 House',
-          name: '909 House',
-        },
-        {
-          id: 'Sampled',
-          name: 'Sampled',
-        },
-        {
-          id: 'Lofi',
-          name: 'Lofi',
-        },
-      ]
-    },
-    {
-      id: 'FX',
-      name: 'FX',
-      children: [
-        {
-          id: '808 Trap',
-          name: '808 Trap',
-        },
-        {
-          id: '909 House',
-          name: '909 House',
-        },
-        {
-          id: 'Sampled',
-          name: 'Sampled',
-        },
-        {
-          id: 'Lofi',
-          name: 'Lofi',
-        },
-      ]
-    },
-  ];
+  {
+    id: "Kicks",
+    name: "Kicks",
+    children: [
+      {
+        id: "808 Trap",
+        name: "808 Trap",
+      },
+      {
+        id: "909 House",
+        name: "909 House",
+      },
+      {
+        id: "Sampled",
+        name: "Sampled",
+      },
+      {
+        id: "Lofi",
+        name: "Lofi",
+      },
+    ],
+  },
+  {
+    id: "Snares",
+    name: "Snares",
+    children: [
+      {
+        id: "808 Trap",
+        name: "808 Trap",
+      },
+      {
+        id: "909 House",
+        name: "909 House",
+      },
+      {
+        id: "Sampled",
+        name: "Sampled",
+      },
+      {
+        id: "Lofi",
+        name: "Lofi",
+      },
+    ],
+  },
+  {
+    id: "Claps",
+    name: "Claps",
+    children: [
+      {
+        id: "808 Trap",
+        name: "808 Trap",
+      },
+      {
+        id: "909 House",
+        name: "909 House",
+      },
+      {
+        id: "Sampled",
+        name: "Sampled",
+      },
+      {
+        id: "Lofi",
+        name: "Lofi",
+      },
+    ],
+  },
+  {
+    id: "Snaps",
+    name: "Snaps",
+    children: [
+      {
+        id: "808 Trap",
+        name: "808 Trap",
+      },
+      {
+        id: "909 House",
+        name: "909 House",
+      },
+      {
+        id: "Sampled",
+        name: "Sampled",
+      },
+      {
+        id: "Lofi",
+        name: "Lofi",
+      },
+    ],
+  },
+  {
+    id: "ClosedHats",
+    name: "Closed Hats",
+    children: [
+      {
+        id: "808 Trap",
+        name: "808 Trap",
+      },
+      {
+        id: "909 House",
+        name: "909 House",
+      },
+      {
+        id: "Sampled",
+        name: "Sampled",
+      },
+      {
+        id: "Lofi",
+        name: "Lofi",
+      },
+    ],
+  },
+  {
+    id: "OpenHats",
+    name: "Open Hats",
+    children: [
+      {
+        id: "808 Trap",
+        name: "808 Trap",
+      },
+      {
+        id: "909 House",
+        name: "909 House",
+      },
+      {
+        id: "Sampled",
+        name: "Sampled",
+      },
+      {
+        id: "Lofi",
+        name: "Lofi",
+      },
+    ],
+  },
+  {
+    id: "Cymbals",
+    name: "Cymbals",
+    children: [
+      {
+        id: "808 Trap",
+        name: "808 Trap",
+      },
+      {
+        id: "909 House",
+        name: "909 House",
+      },
+      {
+        id: "Sampled",
+        name: "Sampled",
+      },
+      {
+        id: "Lofi",
+        name: "Lofi",
+      },
+    ],
+  },
+  {
+    id: "Percs",
+    name: "Percs",
+    children: [
+      {
+        id: "808 Trap",
+        name: "808 Trap",
+      },
+      {
+        id: "909 House",
+        name: "909 House",
+      },
+      {
+        id: "Sampled",
+        name: "Sampled",
+      },
+      {
+        id: "Lofi",
+        name: "Lofi",
+      },
+    ],
+  },
+  {
+    id: "FX",
+    name: "FX",
+    children: [
+      {
+        id: "808 Trap",
+        name: "808 Trap",
+      },
+      {
+        id: "909 House",
+        name: "909 House",
+      },
+      {
+        id: "Sampled",
+        name: "Sampled",
+      },
+      {
+        id: "Lofi",
+        name: "Lofi",
+      },
+    ],
+  },
+];
 
 const useStyles = makeStyles({
   root: {
@@ -219,7 +219,9 @@ export default function RecursiveTreeView() {
 
   const renderTree = (nodes) => (
     <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
-      {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
+      {Array.isArray(nodes.children)
+        ? nodes.children.map((node) => renderTree(node))
+        : null}
     </TreeItem>
   );
 
@@ -227,7 +229,7 @@ export default function RecursiveTreeView() {
     <TreeView
       className={classes.root}
       defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpanded={['root']}
+      defaultExpanded={["root"]}
       defaultExpandIcon={<ChevronRightIcon />}
     >
       {data.map((dataaaaaa) => renderTree(dataaaaaa))}
